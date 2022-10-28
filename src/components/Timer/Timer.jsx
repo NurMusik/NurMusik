a7d9b620e4e06e23990c7b0859a83c6ba5b19f9b
Andrew <andrew.sf.liao@gmail.com>
2022-10-28 14:17:01 -0500

    adding a simple circle timer

--- src/components/Timer/Timer.jsx
+++ src/components/Timer/Timer.jsx
@@ -1,37 +1,34 @@
 import React, { Component } from "react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 
-const Timer = ( {isPlaying, setIsPlaying} ) => {
+const Timer = ({ isPlaying, setIsPlaying }) => {
   return (
     <CountdownCircleTimer
-        isPlaying={isPlaying}
-        duration={5}
-        colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
-        colorsTime={[10, 6, 3, 0]}
-        onComplete={() => {
-          setIsPlaying(false)
-          return { shouldRepeat: false}
-        }
-
-        }
+      isPlaying={isPlaying}
+      duration={5}
+      colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
+      colorsTime={[10, 6, 3, 0]}
+      onComplete={() => {
+        setIsPlaying(false);
+        return { shouldRepeat: false };
+      }}
     >
-     {renderTime}
+      {renderTime}
     </CountdownCircleTimer>
-    );
-}
+  );
+};
 
 const renderTime = ({ remainingTime }) => {
-    if (remainingTime === 0) {
-      return <div className="timer">Done</div>;
-    }
-
-    return (
-      <div className="timer">
-        <div className="text">Audio Remaining</div>
-        <div className="value">{remainingTime}</div>
-        <div className="text">seconds</div>
-      </div>
-    );
-  };
+  if (remainingTime === 0) {
+    return <div className="timer">Done</div>;
+  }
+  return (
+    <div className="timer">
+      <div className="text">Audio Remaining</div>
+      <div className="value">{remainingTime}</div>
+      <div className="text">seconds</div>
+    </div>
+  );
+};
 
 export default Timer;
