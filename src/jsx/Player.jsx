c1597c4db8835188ba81784e4debe35ececd3409
mnijkerk <maximiliannijkerk2021@u.northwestern.edu>
2022-10-28 14:33:53 -0500

    added bootstrap icons

--- src/jsx/Player.jsx
+++ src/jsx/Player.jsx
@@ -1,34 +1,81 @@
 // import { AiFillPlayCircle } from 'react-icons/fa';
 import { useState } from "react";
+import "../css/Player.css";
 
 const Player = () => {
+  const [playState, setPlayState] = useState(true);
 
-    const [playState, setPlayState] = useState(true);
+  const minutes = ["28", "29", "30"];
+  const seconds = ["00", "01", "02"];
+  const [minutesState, setMinutes] = useState("30");
+  const [secondsState, setSeconds] = useState("00");
 
-
-    const PlayBackState = () => {
-        if (playState) {
-            return <i className="bi bi-pause"></i>
-        }
-        else {
-            return <i className="bi bi-play-circle"></i>
-        }
-
+  const PlayBackState = () => {
+    if (playState) {
+      return <i className="bi bi-pause-circle"></i>;
+    } else {
+      return <i className=" bi bi-play-circle"></i>;
     }
+  };
 
-    return (
-        <div className="PlayerPage">
-
-            <div className="Banner"> Banner</div>
-            <div className="RadioInfo"> Radio Information </div>
-            <div className="TimerInfo"> 30:00</div>
-            <div className="PlaybackFunctionality"
-
-                onClick={()=>setPlayState(!playState)}
+  return (
+    <div className="PlayerPage">
+      <div className="Banner"> 
+      <i class="bi bi-arrow-left"></i>
+      <div className="BannerTitle">NurMusik</div>
+      <i class="bi bi-three-dots"></i>
+      
+      </div>
+      <div className="RadioInfo"> Radio Information </div>
+      <div className="TimerInfo">
+        <div>
+          <label style={{ fontWeight: "bold" }}>
+            <select
+              className="mx-2"
+              required={true}
+              value={minutesState}
+              onChange={(e) => {
+                // console.log(e.target.value);
+                setMinutes(e.target.value);
+              }}
             >
-                <PlayBackState></PlayBackState>
-            </div>
-        </div>)
-}
+              {minutes.map((h, id) => (
+                <option key={id} value={h}>
+                  {h}{" "}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label style={{ fontWeight: "bold" }}>
+            <select
+              className="mx-2"
+              required={true}
+              value={secondsState}
+              onChange={(e) => {
+                // console.log(e.target.value);
+                setSeconds(e.target.value);
+              }}
+            >
+              {seconds.map((h, id) => (
+                <option key={id} value={h}>
+                  {h}{" "}
+                </option>
+              ))}
+            </select>
+          </label>
+          
+        </div>
+      </div>
+      <div
+        className="PlaybackFunctionality"
+        onClick={() => setPlayState(!playState)}
+      >
+        <i class="bi nextPrev bi-caret-left-fill"></i>
+        <PlayBackState></PlayBackState>
+        <i class="bi nextPrev bi-caret-right-fill"></i>
+      </div>
+    </div>
+  );
+};
 
-export default Player; 
+export default Player;
