9069c1a464cc452720e321fb954e804ac1ee8c13
abeni3 <abenitamene2023@u.northwestern.edu>
2022-10-28 13:56:17 -0500

    playstate

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Player from './jsx/Player'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const App = () => {
   return (
--- src/jsx/Player.jsx
+++ src/jsx/Player.jsx
@@ -1,12 +1,33 @@
-const Player = () => { 
+// import { AiFillPlayCircle } from 'react-icons/fa';
+import { useState } from "react";
 
-    
+const Player = () => {
 
-    return(<div className="PlayerPage">
+    const [playState, setPlayState] = useState(true);
+
+
+    const PlayBackState = () => {
+        if (playState) {
+            return <i className="bi bi-pause"></i>
+        }
+        else {
+            return <i className="bi bi-play-circle"></i>
+        }
+
+    }
+
+    return (
+        <div className="PlayerPage">
+
             <div className="Banner"> Banner</div>
             <div className="RadioInfo"> Radio Information </div>
             <div className="TimerInfo"> 30:00</div>
-            <div className="PlaybackFunctionality">Start Stop</div>
+            <div className="PlaybackFunctionality"
+
+                onClick={()=>setPlayState(!playState)}
+            >
+                <PlayBackState></PlayBackState>
+            </div>
         </div>)
 }
 
