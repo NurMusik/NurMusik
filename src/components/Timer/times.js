66690b3af1dc6ede773b0451ecd36fce312dc3cd
Yousef Farge <yousef.farge@gmail.com>
2022-10-30 17:28:35 -0500

    modified times.js to account for singular days, hours, minutes, and seconds

--- src/components/Timer/times.js
+++ src/components/Timer/times.js
@@ -2,26 +2,34 @@ const minuteSeconds = 60;
 const hourSeconds = 3600;
 const daySeconds = 86400;
 
-const getTimeDays = (time) => (time / daySeconds) | 0;
+const getTimeDays = (time) => Math.floor(time / daySeconds) | 0;
 const getTimeHours = (time) => Math.floor(time / hourSeconds) | 0;
 const getTimeMinutes = (time) => Math.floor(time / minuteSeconds) | 0;
 
 export const getTimeDimension = (time) => {
-  if (getTimeDays(time) > 1) return "days";
-  if (getTimeHours(time) > 1) return "hours";
-  if (getTimeMinutes(time) > 1) return "minutes";
+  const days = getTimeDays(time);
+  const hours = getTimeHours(time);
+  const minutes = getTimeMinutes(time);
+  if (days > 1) return "days";
+  if (days === 1) return "day";
+  if (hours > 1) return "hours";
+  if (hours === 1) return "hour";
+  if (minutes > 1) return "minutes";
+  if (minutes === 1) return "minute";
+  if (time === 1) return "second";
   return "seconds";
 };
 
 export const formatTime = (dimension, time) => {
-  switch (dimension) {
-    case "days":
+  const d = dimension.at(0);
+  switch (d) {
+    case "d":
       return getTimeDays(time);
-    case "hours":
+    case "h":
       return getTimeHours(time);
-    case "minutes":
+    case "m":
       return getTimeMinutes(time);
-    case "seconds":
+    case "s":
       return time;
     default:
       return time;
